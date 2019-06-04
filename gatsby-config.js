fa2547aae7f384920173df23256a688ab4205150
upex <upenpanging@gmail.com>
2019-06-04 15:53:56 +0530

    meta data added

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,9 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-    title: 'Upen Panging.FullStack Javascript Engineer. Agile minded. Risk taker. Wannabe Chef.',
+    title: `Upen Panging.FullStack Javascript Engineer. Agile minded. Risk taker. Wannabe Chef.`,
+    siteUrl: `https://upenpanging.in/`,
+    description: `FullStack javascript engineer, Agile minded, Risk taker, having keen interest in DevOps and cloud technology, loves minimalist UI/UX design, an entertainer with great sense of humour. My primary focus is on modern Javascript and its ecosystem. Currently learning about GraphQL and loving it.When I'm not coding, you can find me cooking amazing dishes.Frontend Development: HTML5, CSS3, Javascript(ES5/ES6), Angular, Vue, Vuex, React, Redux, GatsbyJs, Material UI, Bootstrap, Vuetify, Jquery, Responsive Design.Backend Development: Nodejs, Express, RESTful API, MongoDB, JSON.Hosting & Deployments: AWS, DigitalOcean, Netlify`,
   },
   plugins: [
     {
