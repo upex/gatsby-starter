6118544583b92a263fd46c950d47ce6194851d61
upen panging <upenpanging@gmaile.com>
2019-06-13 02:00:47 +0530

    profile image optimised

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,7 @@ export const query = graphql`
             thumbnail {
               childImageSharp {
                 fluid(maxWidth: 500, quality: 100) {
-                  ...GatsbyImageSharpFluid
+                  ...GatsbyImageSharpFluid_noBase64
                   presentationWidth
                 }
               }
